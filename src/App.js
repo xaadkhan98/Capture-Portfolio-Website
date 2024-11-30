30f4f4055c5007bea192ccc2fcf0ef2e5ec4d861
xaadkhan98 <xaadkhan98@gmail.com>
2024-11-30 19:35:31 +0500

    9. React Routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OurWork = () => {
+  return (
+    <div>
+      <h1>OOuuurrrr Wrooookkkk</h1>
+    </div>
+  );
+};
+
+export default OurWork;
