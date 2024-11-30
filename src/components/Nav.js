7fa5c4057f2e93e358f3c5efd22592ff81feabb9
xaadkhan98 <xaadkhan98@gmail.com>
2024-11-30 17:03:43 +0500

    8. Creating Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <a href="#" id="logo">
+          Capture
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="#">1. About Us</a>
+        </li>
+        <li>
+          <a href="#">2. Our Work</a>
+        </li>
+        <li>
+          <a href="#">3. Contact Us</a>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: auto;
+  padding: 1rem 10rem;
+  background-color: #282828;
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-weight: lighter;
+    font-family: "Lobster", cursive;
+  }
+`;
+
+export default Nav;
